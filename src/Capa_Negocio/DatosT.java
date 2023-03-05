6d86e49c3137c2d3eb97fccab31177138867ab1e
sebas <sebas@compusebas>
2023-03-05 17:03:57 -0500

    datos

--- src/Capa_Negocio/DatosT.java
+++ src/Capa_Negocio/DatosT.java
@@ -5,5 +5,44 @@ import Capa_Datos.RealG;
 
 
 public class DatosT {
- //a
+    private String Nombre;
+    private int Cedula;
+    private int Celular; 
+    private String Direccion; 
+    
+    
+ 
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(int Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public int getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(int Celular) {
+        this.Celular = Celular;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String Direccion) {
+        this.Direccion = Direccion;
+    }
+    
 }
