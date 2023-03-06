3d4639784824cbe2fb72862c05f8cd9b0558c801
DanM854 <daniel.mora01@correo.usa.edu.co>
2023-03-06 01:59:49 -0500

    no sirve componentes

--- src/Capa_Datos/RealG.java
+++ src/Capa_Datos/RealG.java
@@ -2,8 +2,10 @@ package Capa_Datos;
 import java.sql.*;
 
 
+
 public class RealG 
 {
+    
     private final String url = "jdbc:mysql://localhost:3306/realg";
     private final String user = "root";
     private final String pwd = "@fF.K_JsreC9R0PJ";
@@ -11,36 +13,38 @@ public class RealG
     public RealG()
     {}
     
+    
     public ResultSet Listar(String Cad)
     {
         try
         {
-            Connection cn = DriverManager.getConnection(url, user, pwd);
+            Connection cn = DriverManager.getConnection(url,user,pwd);
             PreparedStatement da = cn.prepareStatement(Cad);
             ResultSet tbl = da.executeQuery();
             return tbl;
         }
         catch(SQLException e)
         {
-            javax.swing.JOptionPane.showMessageDialog(null, e.getMessage());
+            javax.swing.JOptionPane.showMessageDialog(null,e.getMessage());
             return null;
+            
         }
     }
+    
     
     public String Ejecutar(String Cad)
     {
         try
-        {
-            Connection cn = DriverManager.getConnection(url, user, pwd);
-            PreparedStatement da = cn.prepareStatement(Cad);
-            int r = da.executeUpdate();
-            return "Se afectaron " + r + "filas";
-        }
+            {
+                Connection cn = DriverManager.getConnection(url,user,pwd);
+                PreparedStatement da = cn.prepareStatement(Cad);
+                int r = da.executeUpdate();
+                return "Se afectaron " + r + "filas";
+            }
         catch(SQLException e)
-        {
-            javax.swing.JOptionPane.showMessageDialog(null,e.getMessage());
-            return "Error" + e.getMessage();
-        }
+            {
+                javax.swing.JOptionPane.showMessageDialog(null,e.getMessage());
+                return "Error"+e.getMessage();
+            }
     }
-    
 }
